33d1e9516a3905f45067a35ef6157c2beb2d02ee
DrewCort <andrews.cortez@gmail.com>
2021-08-03 01:13:26 +0100

    Correction

--- next.config.js
+++ next.config.js
@@ -2,8 +2,4 @@ module.exports = {
   images: {
     domains: ['images.unsplash.com'],
   },
-  // env: {
-  //   mongodburl:
-  //     'mongodb+srv://dbLaruciAdm:la6413qre$db90@larucidev.kudc4.mongodb.net/larucidev?retryWrites=true&w=majority',
-  // },
 };
